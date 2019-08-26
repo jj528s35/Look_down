0b535da718dc68d9908c16c0bc6f7b91e69469a7
RedFishChang <harris32916@gmail.com>
2019-08-26 18:19:00 +0800

    motor controller

---
+++ look_down/Assets/c#/Motor_controller.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using fofulab;
+
+public class Motor_controller : MonoBehaviour
+{
+    public OutputPin pin;
+    public int value;
+    public PinType type;
+    public int HMIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            print("send message");
+            HapticMakerManager.setValue(HMIndex, pin, value, type);
+        }
+    }
+
+    public void ButtomOnClick_90()
+    {
+        value = 90;
+        print("send message 90");
+        HapticMakerManager.setValue(HMIndex, pin, value, type);
+    }
+
+    public void ButtomOnClick_0()
+    {
+        value = 0;
+        print("send message 0");
+        HapticMakerManager.setValue(HMIndex, pin, value, type);
+    }
+}
